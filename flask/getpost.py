a535dba90c7f78ab2454959f36e46d57db183259
netr212 <netra200021kcbdr@gmail.com>
2025-06-09 12:39:30 +0100

    get in flask

---
+++ flask/getpost.py
@@ -0,0 +1,21 @@
+
+from flask import Flask, render_template
+
+'''
+ It creates an instance of the Flask class, 
+ which will be our WSGI (Web Server Gateway Interface) application.
+'''
+
+### WSGI Application
+app = Flask(__name__)
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5002)
